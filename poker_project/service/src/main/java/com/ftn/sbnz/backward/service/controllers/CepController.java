6bd2bc38726c9b065f22a3dbc9f3cd710a20c05b
Luka Petković <103668292+LukaPetkovic02@users.noreply.github.com>
2025-09-18 11:22:25 +0200

    added bluff event CEP

---
+++ poker_project/service/src/main/java/com/ftn/sbnz/backward/service/controllers/CepController.java
@@ -0,0 +1,23 @@
+package com.ftn.sbnz.backward.service.controllers;
+
+import com.ftn.sbnz.backward.service.services.BluffCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cep")
+public class CepController {
+    private final BluffCepService bluffCepService;
+
+    @Autowired
+    public CepController(BluffCepService bluffCepService) {
+        this.bluffCepService = bluffCepService;
+    }
+
+    @GetMapping("/bluff-detect")
+    public void detectBluffs(){
+        bluffCepService.detectBluffs();
+    }
+}
